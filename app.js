26cd3eb58d76bb0c9656a1357b4a1f2f6a2dfeb5
francoromeo <francoromeo5012@gmail.com>
2023-11-24 20:28:09 -0300

    rotate text

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ let menuIcon = document.querySelector(".menu-icon");
 menuIcon.addEventListener("click",()=>{
     menuIcon.classList.toggle("active")
 })
+
+let text = document.querySelector(".text p");
+
+text.innerHTML = text.innerHTML.split("").map((char,i)=>
+    `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
+).join("")
