503005d64d79691e02eb602bab5b143ed9ac1165
francoromeo <francoromeo5012@gmail.com>
2023-11-25 22:59:00 -0300

    about finishit

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ let text = document.querySelector(".text p");
 text.innerHTML = text.innerHTML.split("").map((char,i)=>
     `<b style="transform:rotate(${i * 6.3}deg")>${char}</b>`
 ).join("")
+
+const buttons = document.querySelectorAll('.about-btn button');
+const contents = document.querySelectorAll('.content');
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        contents.forEach(content => content.style.display = 'none');
+        contents[index].style.display = 'block';
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
