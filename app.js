583876164461d4cc3beeed354b49239906d0cf94
francoromeo <francoromeo5012@gmail.com>
2023-11-29 11:39:49 -0300

    cricle js

--- app.js
+++ app.js
@@ -52,3 +52,45 @@ var swiper = new Swiper(".mySwiper", {
         },
     }
 });
+
+const firstSkill = document.querySelector(".skill:first-child");
+const skCounters = document.querySelectorAll(".counter span");
+const progressBars = document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll",()=>{
+    if(!skillsPlayed)
+    skillsCounter();
+})
+
+function hasReached(el){
+    let topPosition = el.getBoundingClientRect().top;
+    if(window.innerHeight >= topPosition + el.offsetHeight) return true; return false;
+}
+
+function updateCount(num,maxNum){
+    let currentNum = +num.innerText;
+
+    if(currentNum < maxNum){
+        num.innerText = currentNum +1;
+        setTimeout(()=>{
+            updateCount(num, maxNum)
+        },12)
+    }
+}
+
+let skillsPlayed = false;
+
+function skillsCounter(){
+    if(!hasReached(firstSkill))return;
+    skillsPlayed = true;
+    skCounters.forEach((counter, i)=>{
+        let target = +counter.dataset.target;
+        let strokeValue = 465 - 465 * (target / 100);
+        progressBars[i].style.setProperty("--target", strokeValue);
+
+        setTimeout(()=>{
+            updateCount(counter,target);
+        },400)
+    });
+    progressBars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards")
+}
