fe361cacace70a203641da696faa6af1481fe511
francoromeo <francoromeo5012@gmail.com>
2023-11-30 21:27:02 -0300

    active menu

--- app.js
+++ app.js
@@ -116,3 +116,15 @@ let calcScrollValue = ()=>{
 }
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
+
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+function activeMenu() {
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll",activeMenu);
