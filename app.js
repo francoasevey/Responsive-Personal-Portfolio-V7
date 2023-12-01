ee07910104323f0de43e584a27a1aae593706b1b
francoromeo <francoromeo5012@gmail.com>
2023-11-30 21:20:42 -0300

    scroll buttom

--- app.js
+++ app.js
@@ -94,3 +94,25 @@ function skillsCounter(){
     });
     progressBars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards")
 }
+
+let calcScrollValue = ()=>{
+    let scrollProgress = document.getElementById("progress");
+    let pos = document.documentElement.scrollTop;
+
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100)/calcHeight);
+
+    if(pos > 100){
+        scrollProgress.style.display = "grid";
+    }else{
+        scrollProgress.style.display ="none";
+    }
+
+    scrollProgress.addEventListener("click",()=>{
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = `conic-gradient(#fff ${scrollValue}%,#e6006d ${scrollValue}%)`;
+}
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
